7c42938a8d375e1a91b8e6e9de8adb08b6913390
Clemens Schmid <clem.schmid@gmail.com>
2026-09-04 15:44:31 +0200

    add control for y axis scale of 1D plot

--- src/pyFAI/gui/pilx/widgets/IntegratedPatternPlotWidget.py
+++ src/pyFAI/gui/pilx/widgets/IntegratedPatternPlotWidget.py
@@ -35,6 +35,10 @@ __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
 __date__ = "22/03/2024"
 __status__ = "development"
 
+import numpy
+from matplotlib import scale as mscale
+from matplotlib import ticker
+from matplotlib import transforms
 from silx.gui import icons, qt
 from silx.gui.plot import PlotWidget
 from silx.gui.plot.LegendSelector import LegendsDockWidget
@@ -49,6 +53,48 @@ from ..HorizontalRangeROI import HorizontalRangeROI
 from ..models import ROI_COLOR
 from .RoiModeAction import RoiModeAction
 from .RoiRangeWidget import RoiRangeWidget
+
+
+class SignedSquareRootScale(mscale.ScaleBase):
+    """Square-root axis scale which preserves negative values."""
+
+    name = "signed_sqrt"
+
+    def set_default_locators_and_formatters(self, axis):
+        axis.set_major_locator(ticker.AutoLocator())
+        axis.set_major_formatter(ticker.ScalarFormatter())
+        axis.set_minor_locator(ticker.NullLocator())
+        axis.set_minor_formatter(ticker.NullFormatter())
+
+    class Transform(transforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def transform_non_affine(self, values):
+            values = numpy.asarray(values)
+            return numpy.sign(values) * numpy.sqrt(numpy.abs(values))
+
+        def inverted(self):
+            return SignedSquareRootScale.InvertedTransform()
+
+    class InvertedTransform(transforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def transform_non_affine(self, values):
+            values = numpy.asarray(values)
+            return numpy.sign(values) * numpy.square(values)
+
+        def inverted(self):
+            return SignedSquareRootScale.Transform()
+
+    def get_transform(self):
+        return self.Transform()
+
+
+mscale.register_scale(SignedSquareRootScale)
 
 
 class IntegratedPatternPlotWidget(PlotWidget):
@@ -97,6 +143,33 @@ class IntegratedPatternPlotWidget(PlotWidget):
         toolbar.addSeparator()
         toolbar.addAction(PanModeAction(self, toolbar))
         toolbar.addAction(ZoomModeAction(self, toolbar))
+        self._y_scale_button = qt.QToolButton(toolbar)
+        self._y_scale_button.setPopupMode(qt.QToolButton.ToolButtonPopupMode.InstantPopup)
+        y_scale_menu = qt.QMenu(self._y_scale_button)
+        self._y_scale_actions = {}
+        y_scale_group = qt.QActionGroup(self._y_scale_button)
+        y_scale_group.setExclusive(True)
+        for scale, text, icon in (
+            ("linear", "Linear Y-axis", "yscale-linear"),
+            ("log", "Logarithmic Y-axis", "yscale-log"),
+            ("asinh", "Arcsinh Y-axis", "yscale-asinh"),
+            ("signed_sqrt", "Square-root Y-axis", "math-amplitude"),
+        ):
+            action = qt.QAction(icons.getQIcon(icon), text, y_scale_group)
+            action.setCheckable(True)
+            action.triggered.connect(
+                lambda checked=False, selected_scale=scale: self.setYAxisScale(
+                    selected_scale
+                )
+            )
+            y_scale_group.addAction(action)
+            y_scale_menu.addAction(action)
+            self._y_scale_actions[scale] = action
+        self._y_scale_button.setMenu(y_scale_menu)
+        self._y_scale_actions["linear"].setChecked(True)
+        self._y_scale_button.setIcon(icons.getQIcon("yscale-linear"))
+        self._y_scale_button.setToolTip("Y-axis scale is linear")
+        toolbar.addWidget(self._y_scale_button)
         roiAction = RoiModeAction(self, toolbar)
         toolbar.addAction(roiAction)
         # Start in ROI mode
@@ -112,6 +185,32 @@ class IntegratedPatternPlotWidget(PlotWidget):
         refinementAction.triggered.connect(self.refinementRequested)
         toolbar.addAction(refinementAction)
         return toolbar
+
+    def setYAxisScale(self, scale):
+        axis = self.getYAxis()
+        backend = self.getBackend()
+        if scale == "signed_sqrt":
+            if not hasattr(backend, "ax"):
+                raise RuntimeError(
+                    "Square-root Y-axis scaling requires the Matplotlib backend"
+                )
+            if axis.getScale() != "linear":
+                axis.setScale("linear")
+            backend.setYAxisScale("signed_sqrt")
+            icon = "math-amplitude"
+            tooltip = "Y-axis scale is signed square root"
+        else:
+            if axis.getScale() == scale:
+                backend.setYAxisScale(scale)
+            else:
+                axis.setScale(scale)
+            icon = f"yscale-{scale}"
+            tooltip = f"Y-axis scale is {scale}"
+
+        self._y_scale_actions[scale].setChecked(True)
+        self._y_scale_button.setIcon(icons.getQIcon(icon))
+        self._y_scale_button.setToolTip(tooltip)
+        self.resetZoom()
 
     def _initStatusBar(self):
         converters = (
